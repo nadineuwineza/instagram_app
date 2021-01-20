180c58023233d46a7c992e948e5ff040dff1e303
nadineuwineza <nadineuwineza2017@gmail.com>
2021-01-20 09:34:10 +0200

    added test

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,6 +1,22 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+# Create your tests here.
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='nadine')
+        self.user.save()
+
+        self.profile_test = Profile(id=1, name='image', profile_picture='default.png', bio=' test profile',
+                                    user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_test, Profile))
+
+    def test_save_profile(self):
+        self.profile_test.save_profile()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) > 0)
+
